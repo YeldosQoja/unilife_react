5ece80d911033a4e67316d0c9002ecc3ecee7788
Yeldos Kozha <yeldos.kozha@gmail.com>
2022-04-23 17:29:03 +0600

    Added main

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Header title='Header' backgroundColor='red'/>
-      <h1>
-        React App Created!
-      </h1>
-      <h2>
-        Hello, world!
-      </h2>
+      <main>
+        <div className='landing-page'>
+          <img src={require("./images/main-bg.jpg")} alt="" className='main-bg-image'/>
+        </div>
+      </main>
     </div>
   )
 }
